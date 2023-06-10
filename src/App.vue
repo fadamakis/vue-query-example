3298a67269a503c68a0f5b5e38a48cbdb212544e
Fotis Adamakis <fotis316@yahoo.com>
2023-06-10 11:13:08 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ProductsPage />
+</template>
+
+<script setup>
+import ProductsPage from "./pages/ProductsPage.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
